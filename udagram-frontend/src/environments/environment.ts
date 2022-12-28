8a5e73a65e3bf1685b8465ab702a886a67fe1066
datndd260897 <datndd260897@gmail.com>
2022-12-28 16:39:56 +0700

    update env frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'localhost:8080/api/v0'
+  apiHost: 'http://aada01561b1fb425091d3a2e69bb57d0-187007249.us-east-1.elb.amazonaws.com/:8080/api/v0'
 };
 
 /*
